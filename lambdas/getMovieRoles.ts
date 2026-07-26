ed0ebab43cbde8832b30812659f24ba1b9a2ba8d
Briannap33 <20102669@mail.wit.ie>
2026-07-26 20:22:45 +0100

    Add movie roles Lambda handler

---
+++ lambdas/getMovieRoles.ts
@@ -0,0 +1,80 @@
+import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+
+import {
+  DynamoDBDocumentClient,
+  QueryCommand,
+} from "@aws-sdk/lib-dynamodb";
+
+const ddbDocClient = createDocumentClient();
+
+export const handler: APIGatewayProxyHandlerV2 = async (event) => {
+  try {
+    console.log("[EVENT]", JSON.stringify(event));
+
+    const movieID = event.pathParameters?.movieID;
+
+    if (!movieID) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: "Missing movie ID",
+        }),
+      };
+    }
+
+    const commandOutput = await ddbDocClient.send(
+      new QueryCommand({
+        TableName: process.env.TABLE_NAME,
+
+        KeyConditionExpression:
+          "PK = :pk AND begins_with(SK, :actorPrefix)",
+
+        ExpressionAttributeValues: {
+          ":pk": `m#${movieID}`,
+          ":actorPrefix": "a#",
+        },
+      })
+    );
+
+    return {
+      statusCode: 200,
+
+      headers: {
+        "content-type": "application/json",
+      },
+
+      body: JSON.stringify({
+        roles: commandOutput.Items,
+      }),
+    };
+  } catch (error: any) {
+    console.log(JSON.stringify(error));
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error }),
+    };
+  }
+};
+
+function createDocumentClient() {
+  const ddbClient = new DynamoDBClient({
+    region: process.env.REGION,
+  });
+
+  const marshallOptions = {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true,
+  };
+
+  const unmarshallOptions = {
+    wrapNumbers: false,
+  };
+
+  return DynamoDBDocumentClient.from(ddbClient, {
+    marshallOptions,
+    unmarshallOptions,
+  });
+}
