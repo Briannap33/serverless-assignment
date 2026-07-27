0103b28e51c04966dfc1f69278a0b0892830f03d
Briannap33 <20102669@mail.wit.ie>
2026-07-27 20:06:21 +0100

    Connect movie roles endpoint to API Gateway

--- lib/serverless-assignment-stack.ts
+++ lib/serverless-assignment-stack.ts
@@ -2,6 +2,10 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as custom from "aws-cdk-lib/custom-resources";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as lambdanode from "aws-cdk-lib/aws-lambda-nodejs";
+import * as apig from "aws-cdk-lib/aws-apigateway";
+
 import { seedData } from "../seed/data";
 import { generateBatch } from "../shared/util";
 
@@ -40,6 +44,63 @@ export class ServerlessAssignmentStack extends cdk.Stack {
       policy: custom.AwsCustomResourcePolicy.fromSdkCalls({
         resources: [movieCastTable.tableArn],
       }),
+    });
+
+    const getMovieRolesFn =
+      new lambdanode.NodejsFunction(
+        this,
+        "GetMovieRolesFn",
+        {
+          architecture: lambda.Architecture.ARM_64,
+          runtime: lambda.Runtime.NODEJS_22_X,
+
+          entry:
+            `${__dirname}/../lambdas/getMovieRoles.ts`,
+
+          timeout: cdk.Duration.seconds(10),
+          memorySize: 128,
+
+          environment: {
+            TABLE_NAME: movieCastTable.tableName,
+            REGION: "eu-west-1",
+          },
+        }
+      );
+
+    movieCastTable.grantReadData(getMovieRolesFn);
+
+    const api = new apig.RestApi(this, "MovieCastApi", {
+      description: "Movie Cast REST API",
+
+      deployOptions: {
+        stageName: "dev",
+      },
+
+      defaultCorsPreflightOptions: {
+        allowOrigins: apig.Cors.ALL_ORIGINS,
+        allowMethods: apig.Cors.ALL_METHODS,
+      },
+    });
+
+    const moviesEndpoint =
+      api.root.addResource("movies");
+
+    const specificMovieEndpoint =
+      moviesEndpoint.addResource("{movieID}");
+
+    const rolesEndpoint =
+      specificMovieEndpoint.addResource("roles");
+
+    rolesEndpoint.addMethod(
+      "GET",
+      new apig.LambdaIntegration(
+        getMovieRolesFn,
+        { proxy: true }
+      )
+    );
+
+    new cdk.CfnOutput(this, "MovieCastApiUrl", {
+      value: api.url,
     });
   }
 }
