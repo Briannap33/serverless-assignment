68975d96114b9417736c887765f64104691e75c9
Briannap33 <20102669@mail.wit.ie>
2026-08-03 21:22:50 +0100

    Add signup request validation

--- lambdas/auth/signup.ts
+++ lambdas/auth/signup.ts
@@ -1,14 +1,67 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 import { SignUpBody } from "../../shared/types";
-import {CognitoIdentityProviderClient,SignUpCommand,SignUpCommandInput,} from "@aws-sdk/client-cognito-identity-provider";
+
 import Ajv from "ajv";
 import schema from "../../shared/types.schema.json";
 
 const ajv = new Ajv();
-const isValidBodyParams = ajv.compile(
-  schema.definitions["SignUpBody"] || {}
-);
 
-const client = new CognitoIdentityProviderClient({
-  region: process.env.REGION,
-});
+const isValidBodyParams =
+  ajv.compile(
+    schema.definitions["SignUpBody"] || {}
+  );
+
+export const handler: APIGatewayProxyHandlerV2 = async (event) => {
+  try {
+    console.log("[EVENT]", JSON.stringify(event));
+
+    const body =
+      event.body
+        ? JSON.parse(event.body)
+        : undefined;
+
+    if (!isValidBodyParams(body)) {
+      return {
+        statusCode: 500,
+
+        headers: {
+          "content-type": "application/json",
+        },
+
+        body: JSON.stringify({
+          message:
+            "Incorrect type. Must match SignUpBody schema",
+
+          schema:
+            schema.definitions["SignUpBody"],
+        }),
+      };
+    }
+
+    const signUpBody =
+      body as SignUpBody;
+
+    return {
+      statusCode: 200,
+
+      headers: {
+        "content-type": "application/json",
+      },
+
+      body: JSON.stringify({
+        message: "Signup body is valid",
+      }),
+    };
+
+  } catch (err) {
+    console.error(err);
+
+    return {
+      statusCode: 500,
+
+      body: JSON.stringify({
+        message: err,
+      }),
+    };
+  }
+};
