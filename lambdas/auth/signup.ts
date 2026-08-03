6ad4388f190085eb9577173f63229371ba1884e4
Briannap33 <20102669@mail.wit.ie>
2026-08-03 23:08:09 +0100

    Complete Cognito signup operation

--- lambdas/auth/signup.ts
+++ lambdas/auth/signup.ts
@@ -3,65 +3,84 @@ import { SignUpBody } from "../../shared/types";
 
 import Ajv from "ajv";
 import schema from "../../shared/types.schema.json";
+import {CognitoIdentityProviderClient,SignUpCommand,SignUpCommandInput,} from "@aws-sdk/client-cognito-identity-provider";
 
 const ajv = new Ajv();
 
 const isValidBodyParams =
-  ajv.compile(
-    schema.definitions["SignUpBody"] || {}
-  );
+    ajv.compile(
+        schema.definitions["SignUpBody"] || {}
+    );
+    
+const client =
+  new CognitoIdentityProviderClient({
+    region: process.env.REGION,
+  });
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
-  try {
-    console.log("[EVENT]", JSON.stringify(event));
+    try {
+        console.log("[EVENT]", JSON.stringify(event));
 
-    const body =
-      event.body
-        ? JSON.parse(event.body)
-        : undefined;
+        const body =
+            event.body
+                ? JSON.parse(event.body)
+                : undefined;
 
-    if (!isValidBodyParams(body)) {
-      return {
-        statusCode: 500,
+        if (!isValidBodyParams(body)) {
+            return {
+                statusCode: 500,
 
-        headers: {
-          "content-type": "application/json",
-        },
+                headers: {
+                    "content-type": "application/json",
+                },
 
-        body: JSON.stringify({
-          message:
-            "Incorrect type. Must match SignUpBody schema",
+                body: JSON.stringify({
+                    message:
+                        "Incorrect type. Must match SignUpBody schema",
 
-          schema:
-            schema.definitions["SignUpBody"],
-        }),
-      };
+                    schema:
+                        schema.definitions["SignUpBody"],
+                }),
+            };
+        }
+
+        const signUpBody =
+            body as SignUpBody;
+
+        const params: SignUpCommandInput = {
+            ClientId: process.env.CLIENT_ID!,
+            Username: signUpBody.username,
+            Password: signUpBody.password,
+            UserAttributes: [
+                {
+                    Name: "email",
+                    Value: signUpBody.email,
+                },
+            ],
+        };
+
+        const command =
+            new SignUpCommand(params);
+
+        const res =
+            await client.send(command);
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: res,
+            }),
+        };
+
+    } catch (err) {
+        console.error(err);
+
+        return {
+            statusCode: 500,
+
+            body: JSON.stringify({
+                message: err,
+            }),
+        };
     }
-
-    const signUpBody =
-      body as SignUpBody;
-
-    return {
-      statusCode: 200,
-
-      headers: {
-        "content-type": "application/json",
-      },
-
-      body: JSON.stringify({
-        message: "Signup body is valid",
-      }),
-    };
-
-  } catch (err) {
-    console.error(err);
-
-    return {
-      statusCode: 500,
-
-      body: JSON.stringify({
-        message: err,
-      }),
-    };
-  }
 };
