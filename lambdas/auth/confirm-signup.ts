4c1974b0035a6cb1907a9947d131115d7c03fcc1
Briannap33 <20102669@mail.wit.ie>
2026-08-04 19:27:42 +0100

    Add signup confirmation validation

--- lambdas/auth/confirm-signup.ts
+++ lambdas/auth/confirm-signup.ts
@@ -5,6 +5,7 @@ import Ajv from "ajv";
 import schema from "../../shared/types.schema.json";
 
 const ajv = new Ajv();
+
 const isValidBodyParams = ajv.compile(
   schema.definitions["ConfirmSignUpBody"] || {}
 );
@@ -12,3 +13,48 @@ const isValidBodyParams = ajv.compile(
 const client = new CognitoIdentityProviderClient({
   region: process.env.REGION,
 });
+
+export const handler: APIGatewayProxyHandlerV2 = async (event) => {
+  try {
+    console.log("[EVENT]", JSON.stringify(event));
+
+    const body = event.body
+      ? JSON.parse(event.body)
+      : undefined;
+
+    if (!isValidBodyParams(body)) {
+      return {
+        statusCode: 500,
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          message:
+            "Incorrect type. Must match ConfirmSignUpBody schema",
+          schema:
+            schema.definitions["ConfirmSignUpBody"],
+        }),
+      };
+    }
+
+    const confirmSignUpBody =
+      body as ConfirmSignUpBody;
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "Confirmation body is valid",
+      }),
+    };
+
+  } catch (err) {
+    console.error(err);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: err,
+      }),
+    };
+  }
+};
