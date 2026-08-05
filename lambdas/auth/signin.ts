dac3f6fa1c60e90a1f7f36c5ba3d40e6988b0487
Briannap33 <20102669@mail.wit.ie>
2026-08-05 18:11:30 +0100

    Add signin request validation

--- lambdas/auth/signin.ts
+++ lambdas/auth/signin.ts
@@ -1,14 +1,60 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
 import { SignInBody } from "../../shared/types";
-import {CognitoIdentityProviderClient,InitiateAuthCommand,InitiateAuthCommandInput,} from "@aws-sdk/client-cognito-identity-provider";
+import { CognitoIdentityProviderClient, InitiateAuthCommand, InitiateAuthCommandInput, } from "@aws-sdk/client-cognito-identity-provider";
 import Ajv from "ajv";
 import schema from "../../shared/types.schema.json";
 
 const ajv = new Ajv();
+
 const isValidBodyParams = ajv.compile(
-  schema.definitions["SignInBody"] || {}
+    schema.definitions["SignInBody"] || {}
 );
 
 const client = new CognitoIdentityProviderClient({
-  region: process.env.REGION,
+    region: process.env.REGION,
 });
+
+export const handler: APIGatewayProxyHandlerV2 = async (event) => {
+    try {
+        console.log("[EVENT]", JSON.stringify(event));
+
+        const body = event.body
+            ? JSON.parse(event.body)
+            : undefined;
+
+        if (!isValidBodyParams(body)) {
+            return {
+                statusCode: 500,
+                headers: {
+                    "content-type": "application/json",
+                },
+                body: JSON.stringify({
+                    message:
+                        "Incorrect type. Must match SignInBody schema",
+                    schema:
+                        schema.definitions["SignInBody"],
+                }),
+            };
+        }
+
+        const signInBody =
+            body as SignInBody;
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: "Signin body is valid",
+            }),
+        };
+
+    } catch (err) {
+        console.error(err);
+
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: err,
+            }),
+        };
+    }
+};
