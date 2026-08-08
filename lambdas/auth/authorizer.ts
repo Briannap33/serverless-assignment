e630548e970a8d8682cceb797b06b9a0d67aa2fc
Briannap33 <20102669@mail.wit.ie>
2026-08-08 15:06:41 +0100

    Deny requests without Cognito token

--- lambdas/auth/authorizer.ts
+++ lambdas/auth/authorizer.ts
@@ -1,2 +1,36 @@
 import {APIGatewayRequestAuthorizerHandler,} from "aws-lambda";
 import {createPolicy,parseCookies,verifyToken,} from "../utils";
+
+export const handler:
+  APIGatewayRequestAuthorizerHandler =
+  async (event) => {
+
+    console.log(
+      "[EVENT]",
+      JSON.stringify(event)
+    );
+
+    const cookies =
+      parseCookies(event);
+
+    if (!cookies?.token) {
+      return {
+        principalId: "",
+
+        policyDocument:
+          createPolicy(
+            event,
+            "Deny"
+          ),
+      };
+    }
+
+    return {
+      principalId: "",
+      policyDocument:
+        createPolicy(
+          event,
+          "Deny"
+        ),
+    };
+  };
