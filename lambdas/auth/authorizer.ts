cb56a3b5e41ac2fecabf804cba12f5399269ee56
Briannap33 <20102669@mail.wit.ie>
2026-08-08 16:57:45 +0100

    Verify Cognito token in authorizer

--- lambdas/auth/authorizer.ts
+++ lambdas/auth/authorizer.ts
@@ -1,36 +1,56 @@
-import {APIGatewayRequestAuthorizerHandler,} from "aws-lambda";
-import {createPolicy,parseCookies,verifyToken,} from "../utils";
+import { APIGatewayRequestAuthorizerHandler, } from "aws-lambda";
+import { createPolicy, parseCookies, verifyToken, } from "../utils";
 
 export const handler:
-  APIGatewayRequestAuthorizerHandler =
-  async (event) => {
+    APIGatewayRequestAuthorizerHandler =
+    async (event) => {
 
-    console.log(
-      "[EVENT]",
-      JSON.stringify(event)
-    );
+        console.log(
+            "[EVENT]",
+            JSON.stringify(event)
+        );
 
-    const cookies =
-      parseCookies(event);
+        const cookies =
+            parseCookies(event);
 
-    if (!cookies?.token) {
-      return {
-        principalId: "",
+        if (!cookies?.token) {
+            return {
+                principalId: "",
+                policyDocument:
+                    createPolicy(
+                        event,
+                        "Deny"
+                    ),
+            };
+        }
 
-        policyDocument:
-          createPolicy(
-            event,
-            "Deny"
-          ),
-      };
-    }
+        const verifiedJwt =
+            await verifyToken(
+                cookies.token,
+                process.env.USER_POOL_ID,
+                process.env.REGION!
+            );
 
-    return {
-      principalId: "",
-      policyDocument:
-        createPolicy(
-          event,
-          "Deny"
-        ),
+        if (!verifiedJwt) {
+            return {
+                principalId: "",
+
+                policyDocument:
+                    createPolicy(
+                        event,
+                        "Deny"
+                    ),
+            };
+        }
+
+        return {
+            principalId:
+                verifiedJwt.sub,
+
+            policyDocument:
+                createPolicy(
+                    event,
+                    "Deny"
+                ),
+        };
     };
-  };
